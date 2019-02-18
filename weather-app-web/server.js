6fa6494bbab1f123d24f14a0f535c311f3b47dc3
Beverly Pham <beverly.pham@gmail.com>
2019-02-17 18:05:29 -0800

    post route written and api returning valid data

--- weather-app-web/server.js
+++ weather-app-web/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
+
+const apiKey = process.env.APIKEY;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,8 +14,22 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-  console.log(req.body.city);
-  res.render('index');
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+
+  request(url, function(err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Sorry, there was an error. Please try again'});
+    } else {
+      let weather = JSON.parse(body);
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Invalid city, please try again'});
+      } else {
+        let weatherText = `It is ${weather.main.temp.toFixed(0)} degrees F in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 });
 
 app.listen(3000, function() {
