16ffcc7c01bae1ce04de336b9f3e1ead108bbf96
Beverly Pham <beverly.pham@gmail.com>
2019-02-17 17:40:59 -0800

    user body-parser to grab input field of form

--- weather-app-web/server.js
+++ weather-app-web/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.post('/', function(req, res) {
+  console.log(req.body.city);
   res.render('index');
 });
 
