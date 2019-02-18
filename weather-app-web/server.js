934aa218189db94071178b31cdc7e7050184108c
Beverly Pham <beverly.pham@gmail.com>
2019-02-17 17:36:56 -0800

    index page loads, need to connect form

--- weather-app-web/server.js
+++ weather-app-web/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.listen(3000, function() {
