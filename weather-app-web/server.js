5049ed4331f878c02130749e138f7c675c537993
Beverly Pham <beverly.pham@gmail.com>
2019-02-17 18:51:44 -0800

    fix initial page load app.get()

--- weather-app-web/server.js
+++ weather-app-web/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {weather: null, error: null});
 });
 
 app.post('/', function(req, res) {
